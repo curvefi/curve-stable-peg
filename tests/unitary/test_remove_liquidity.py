d92e7a5ae950106d9d099383d9f033185b8c078a
Roman Agureev <Agureev.RS@phystech.edu>
2021-08-30 14:38:53 +0300

    chore: add remove_liquidity tests

--- tests/unitary/test_remove_liquidity.py
+++ tests/unitary/test_remove_liquidity.py
@@ -15,14 +15,75 @@ def _provide_liquidity_and_token(pool, peg, pegged, alice, pool_token, peg_keepe
     pool_token.transfer(peg_keeper, lp_amount // 2, {"from": alice})
 
 
-def _balance_change(pool, peg, pegged, peg_keeper, admin, diff):
+def _balance_change_withdraw(pool, peg, pegged, peg_keeper, admin, diff):
     assert peg_keeper.update({"from": admin}).return_value
 
     assert int(peg.balanceOf(pool)) == approx(pegged.balanceOf(pool) - (diff - diff // 5))
     assert int(pool.balances(0)) == approx(pool.balances(1) - (diff - diff // 5), abs=diff * pool.fee())
 
 
-def test_remove_liquidity_peg(_provide_liquidity_and_token, pool, pool_token, peg, pegged, alice, peg_keeper, admin):
-    amount = pool.remove_liquidity_one_coin(pool_token.balanceOf(alice), 0, 0, {"from": alice}).return_value
+def _balance_change_provide(pool, peg, pegged, peg_keeper, admin, diff):
+    assert peg_keeper.update({"from": admin}).return_value
 
-    _balance_change(pool, peg, pegged, peg_keeper, admin, amount)
+    assert int(peg.balanceOf(pool)) == approx(pegged.balanceOf(pool) + (diff - diff // 5))
+    assert int(pool.balances(0)) == approx(pool.balances(1) + (diff - diff // 5), abs=diff * pool.fee())
+
+
+def _balance_do_not_change(pool, peg, pegged, peg_keeper, admin):
+    balances = [pool.balances(0), pool.balances(1)]
+    real_balances = [peg.balanceOf(pool), pegged.balanceOf(pool)]
+
+    assert not peg_keeper.update({"from": admin}).return_value
+
+    assert balances == [pool.balances(0), pool.balances(1)]
+    assert real_balances == [peg.balanceOf(pool), pegged.balanceOf(pool)]
+
+
+def test_remove_liquidity_peg(_provide_liquidity_and_token, pool, pool_token, peg, pegged, alice, peg_keeper, admin):
+    """ Remove [x, 0] """
+    amount = pool.remove_liquidity_one_coin(
+        pool_token.balanceOf(alice),
+        0,  # coin
+        0,  # min_amount
+        {"from": alice},
+    ).return_value
+    _balance_change_withdraw(pool, peg, pegged, peg_keeper, admin, amount)
+
+
+def test_remove_liquidity_pegged(_provide_liquidity_and_token, pool, pool_token, peg, pegged, alice, peg_keeper, admin):
+    """ Remove [0, x] """
+    amount = pool.remove_liquidity_one_coin(
+        pool_token.balanceOf(alice),
+        1,  # coin
+        0,  # min_amount
+        {"from": alice},
+    ).return_value
+    _balance_change_provide(pool, peg, pegged, peg_keeper, admin, amount)
+
+
+def test_remove_liquidity_equal(_provide_liquidity_and_token, pool, pool_token, peg, pegged, alice, peg_keeper, admin):
+    """ Remove [x, x] """
+    pool.remove_liquidity(pool_token.balanceOf(alice), [0, 0], {"from": alice})
+    _balance_do_not_change(pool, peg, pegged, peg_keeper, admin)
+
+
+def test_remove_liquidity_more_peg(_provide_liquidity_and_token, pool, pool_token, peg, pegged, alice, peg_keeper, admin):
+    """ Remove [2 * x, x] """
+    token_balance = pool_token.balanceOf(alice)
+    # Balances in the pool are equal
+    amount = pool.calc_withdraw_one_coin(2 * token_balance // 3, 0, {"from": alice})
+    pool.remove_liquidity_imbalance([amount, amount // 2], token_balance, {"from": alice})
+
+    diff = amount - amount // 2
+    _balance_change_withdraw(pool, peg, pegged, peg_keeper, admin, diff)
+
+
+def test_remove_liquidity_more_pegged(_provide_liquidity_and_token, pool, pool_token, peg, pegged, alice, peg_keeper, admin):
+    """ Remove [x, 2 * x] """
+    token_balance = pool_token.balanceOf(alice)
+    # Balances in the pool are equal
+    amount = pool.calc_withdraw_one_coin(2 * token_balance // 3, 1, {"from": alice})
+    pool.remove_liquidity_imbalance([amount // 2, amount], token_balance, {"from": alice})
+
+    diff = amount - amount // 2
+    _balance_change_provide(pool, peg, pegged, peg_keeper, admin, diff)
