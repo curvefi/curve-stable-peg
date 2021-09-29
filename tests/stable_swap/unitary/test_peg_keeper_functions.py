004b42967f28f2234ebb80c8da7aa2d9de3a488c
Makeev Ivan <makeev@phystech.edu>
2021-09-30 00:19:46 +0300

    test: remove old tests

--- tests/stable_swap/unitary/test_peg_keeper_functions.py
+++ tests/stable_swap/unitary/test_peg_keeper_functions.py
@@ -50,28 +50,3 @@ def test_remove(swap, peg, pegged, initial_amounts, peg_keeper):
 def test_remove_access(swap, bob):
     with brownie.reverts("Callable only by Peg Keeper"):
         swap.peg_keeper_remove(100, {"from": bob})
-
-
-def test_remove_via_token(swap, peg, pegged, pool_token, peg_keeper):
-    token_amount = pool_token.balanceOf(peg_keeper)
-
-    balances = [swap.balances(0), swap.balances(1)]
-    real_balances = [peg.balanceOf(swap), pegged.balanceOf(swap)]
-
-    amount = swap.peg_keeper_remove_via_token(
-        token_amount, {"from": peg_keeper}
-    ).return_value
-    assert amount > 0
-
-    new_balances = [swap.balances(0), swap.balances(1)]
-    new_real_balances = [peg.balanceOf(swap), pegged.balanceOf(swap)]
-    assert new_balances[0] == balances[0]
-    assert int(new_balances[1]) == balances[1] - amount
-
-    assert new_real_balances[0] == real_balances[0]
-    assert new_real_balances[1] == real_balances[1] - amount
-
-
-def test_remove_via_token_access(swap, bob):
-    with brownie.reverts("Callable only by Peg Keeper"):
-        swap.peg_keeper_remove_via_token(100, {"from": bob})
